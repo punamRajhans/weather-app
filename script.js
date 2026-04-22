30176d9d80f43d863796a0200af55cec3b2ea534
punamRajhans <rajhanspunam25@gmail.com>
2026-04-22 17:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", searchForLocation);
 
 let target = "Lucknow";
 const fetchresult = async (targetLocation) => {
-  let url = `http://api.weatherapi.com/v1/current.json?key=6eab9411b9694ea5bb271054261502&q=${targetLocation}&aqi=no`;
+  let url = `https://api.weatherapi.com/v1/current.json?key=6eab9411b9694ea5bb271054261502&q=${targetLocation}&aqi=no`;
 
   const res = await fetch(url);
   const data = await res.json();
